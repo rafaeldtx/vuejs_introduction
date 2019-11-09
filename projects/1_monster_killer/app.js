91fa073a2b88214543859d9c3f379e49e44b7965
rafaeldtx <rafael991_@hotmail.com>
2019-11-09 09:32:24 -0300

    Add functions to buttons

--- projects/1_monster_killer/app.js
+++ projects/1_monster_killer/app.js
@@ -1,6 +1,42 @@
 new Vue({
   el: '#app',
+  data: {
+    hero: 100,
+    monster: 100,
+    default_life: 100,
+    logs: []
+  },
+  computed: {
+
+  },
+  watch: {
+  },
   methods: {
-    
+    attack() {
+      this.battle('minus', { min: 6, max: 11 }, 'hero')
+      this.battle('minus', { min: 3, max: 7 }, 'monster')
+    },
+    special_attack() {
+      this.battle('minus', { min: 3, max: 7 }, 'hero')
+      this.battle('minus', { min: 6, max: 11 }, 'monster')
+    },
+    health() {
+      this.battle('plus', { min: 6, max: 11 }, 'hero')
+      this.battle('minus', { min: 3, max: 11 }, 'hero')
+    },
+    battle(type, range, target) {
+      math = Math.floor(Math.random() * (range.max - range.min + 1) + range.min);
+
+      if (type == 'minus') 
+        if((this[target] - math) < 0)
+          this[target] = 0
+        else
+          this[target]-= math
+      else
+        if((this[target] + math) > 100)
+          this[target] = 100
+        else
+          this[target] += math
+    },
   }
 })
