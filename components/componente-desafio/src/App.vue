12273d10bb5d5f06c63d937339ba83005c269ade
rafaeldtx <rafael991_@hotmail.com>
2019-11-14 10:34:25 -0300

    componentized project

--- components/componente-desafio/src/App.vue
+++ components/componente-desafio/src/App.vue
@@ -1,55 +1,30 @@
 <template>
 	<v-app>
-		<v-navigation-drawer app>
-			<v-toolbar flat>
-				<v-list>
-					<v-list-tile>
-						<v-list-tile-title class="title">
-							Menu
-						</v-list-tile-title>
-					</v-list-tile>
-				</v-list>
-			</v-toolbar>
+		<app-menu />
 
-			<v-divider></v-divider>
-
-			<v-list dense class="pt-0">
-				<v-list-tile v-for="item in itensMenu" :key="item.titulo">
-					<v-list-tile-action>
-						<v-icon>{{ item.icon }}</v-icon>
-					</v-list-tile-action>
-					<v-list-tile-content>
-						<v-list-tile-title>{{ item.titulo }}</v-list-tile-title>
-					</v-list-tile-content>
-				</v-list-tile>
-			</v-list>
-		</v-navigation-drawer>
-
-		<v-toolbar app>
-			<v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-			</v-toolbar-title>
-		</v-toolbar>
+		<app-toolbar />
 
 		<v-content>
-			<v-carousel>
-				<v-carousel-item
-					v-for="(item,i) in fotos"
-					:key="i"
-					:src="item.src"></v-carousel-item>
-			</v-carousel>
+			<app-carousel />
 		</v-content>
 
-		<v-footer class="pa-3" app>
-			<v-spacer></v-spacer>
-			<div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-		</v-footer>
+		<app-footer />
 	</v-app>
 </template>
 
 <script>
+import Toolbar from './Toolbar.vue'
+import Menu from './Menu.vue'
+import Carousel from './Carousel.vue'
+import Footer from './Footer.vue'
+
 export default {
+	components: {
+		'app-footer': Footer,
+		'app-carousel': Carousel,
+		'app-toolbar': Toolbar,
+		'app-menu': Menu,
+	},
 	data() {
 		return {
 			itensMenu: [
