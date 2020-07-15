c28d96a1dac001c56bf3fda922b8a412ff0107f8
Rafael Domingues <rafael991_@hotmail.com>
2020-07-14 21:18:52 -0300

    (Mixins) Add base style to Fruit.vue

--- filtros-mixins/src/components/Fruit.vue
+++ filtros-mixins/src/components/Fruit.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="fruit">
-        <ul class="list">
+        <div class="input-group">
+            <input class="input" type="text" v-model="fruit" @keydown.enter="add"/>
+            <button class="add-button" @click="add">Add Fruit</button>
+        </div>
+
+        <ul class="list" v-if="fruits.length">
             <li v-for="fruit in fruits" :key="fruit">
                 {{ fruit }}
             </li>
@@ -13,7 +18,7 @@ export default {
     data() {
         return {
             fruit: null,
-            fruits: ['banana', 'maçã', 'laranja']
+            fruits: []
         }
     },
     methods: {
@@ -28,7 +33,39 @@ export default {
 </script>
 
 <style scoped>
-    .fruit .list {
-        list-style: none;
-    }
+.fruit {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+.fruit .list {
+    list-style: none;
+    background-color: green;
+    color: white;
+    padding: 1.5rem;
+    border-radius: 8px;
+    max-width: 250px;
+}
+
+.input {
+    background-color: rgb(200, 238, 194);
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+    font-size: 1.2rem;
+    border: 1px solid gray;
+    border-right: 0px;
+    padding: 0.8rem 0.3rem;
+}
+
+.add-button {
+    background-color: rgb(90, 173, 90);
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 1.2rem;
+    border: 1px solid gray;
+    padding: 0.8rem 0.3rem;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
 </style>
