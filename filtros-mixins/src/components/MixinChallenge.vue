b815a9118f2f8f0a13f19a5795191ed9f4c25d31
Rafael Domingues <rafael991_@hotmail.com>
2020-07-14 22:26:09 -0300

    (MixinChallenge) Add computed examples

--- filtros-mixins/src/components/MixinChallenge.vue
+++ filtros-mixins/src/components/MixinChallenge.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
       <h3>{{ fruits | words_length | space_to_comma }}</h3>
+      <h3>{{ using_words_length }}</h3>
+      <h3>{{ using_space_to_comma }}</h3>
   </div>
 </template>
 
@@ -8,13 +10,21 @@
 export default {
     filters: {
         space_to_comma(sentence) {
-            return sentence.split(' ').join(',')
+            return sentence.replace(/\s/g, ',')
         }
     },
     data () {
          return {
              fruits: 'Maçã Pera Melancia Abacate'
          }
+    },
+    computed: {
+        using_words_length() {
+            return this.fruits.replace(/\s/g, ',')
+        },
+        using_space_to_comma() {
+            return this.fruits.split(' ').map(fruit => `${fruit} (${fruit.length})`).join(' ')
+        }
     }
 }
 </script>
