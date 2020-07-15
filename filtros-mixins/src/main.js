d6eced94e2c2b26c28e27cecb803198eac15337a
Rafael Domingues <rafael991_@hotmail.com>
2020-07-14 21:42:07 -0300

    (Mixin) Add example of global mixin

--- filtros-mixins/src/main.js
+++ filtros-mixins/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.mixin({
+    created() {
+        console.log('Mixin Global on created!')
+    }
+})
+
 new Vue({
 	render: h => h(App)
 }).$mount('#app')
