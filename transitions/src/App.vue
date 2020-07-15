4cfb44387fd156ac5a262d0ea6dea6ce8b89ddc2
Rafael Domingues <rafael991_@hotmail.com>
2020-07-15 20:20:52 -0300

    (Transition) Add css transition opacity

--- transitions/src/App.vue
+++ transitions/src/App.vue
@@ -3,7 +3,7 @@
 		<h1>Animações</h1>
         <hr>
         <b-button variant="primary" @click="show = !show" class="mb-3">Show message</b-button>
-        <transition>
+        <transition name="fade">
             <b-alert variant="primary" show v-if="show">This is a bootstrap message with toggle animation</b-alert>
         </transition>
 	</div>
@@ -29,5 +29,30 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+/* INITIAL ANIMATION STATE */
+.fade-enter {
+    opacity: 0;
+}
+/* DURING ANIMATION STATE */
+.fade-enter-active {
+   transition: opacity 2s;
+}
+/* FINISHED ANIMATION STATE */
+.face-enter-to {
+    opacity: 1;
+}
+
+/* INITIAL LEAVE ANIMATION STATE */
+.fade-leave {
+    opacity: 1;
+}
+/* DURING LEAVE ANIMATION STATE */
+.fade-leave-active {
+   transition: opacity 2s;
+}
+/* FINISHED LEAVE ANIMATION STATE */
+.face-leave-to {
+    opacity: 0;
 }
 </style>
