b4be23c7bc1398fcec6414ac157124f933702c2e
Rafael Domingues <rafael991_@hotmail.com>
2020-07-31 22:02:54 -0300

    (http-request) creating basic form

--- http-requests/src/App.vue
+++ http-requests/src/App.vue
@@ -1,12 +1,62 @@
 <template>
 	<div id="app" class="container">
-		<h1>HTTP com Axios</h1>
+		<h1>HTTP with Axios</h1>
+
+        <b-card>
+            <b-form-group label="Name:">
+                <b-form-input
+                    type="text"
+                    size="lg"
+                    v-model="user.name"
+                    placeholder="Input your name"
+                >
+                </b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Email:">
+                <b-form-input
+                    type="email"
+                    size="lg"
+                    v-model="user.email"
+                    placeholder="Input your email"
+                >
+                </b-form-input>
+            </b-form-group>
+
+            <hr>
+
+            <b-button
+                @click.prevent="save"
+                size="lg"
+                variant="primary"
+            >
+            Save
+            </b-button>
+        </b-card>
 	</div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            user:{
+                name: '',
+                email: ''
+            }
+        }
+    },
+    methods: {
+        save() {
+            console.log(this.user)
+        }
+    }
+    // created() {
+    //     this.$http.post('usuarios.json', {
+    //         name: 'rafael',
+    //         email: 'rafael@example.com'
+    //     }).then(res => console.log(res))
+    // }
 }
 </script>
 
