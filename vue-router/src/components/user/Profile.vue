57c5c6ce009d51f12e5a8e8135a6ada8d871918a
Rafael Domingues <rafael991_@hotmail.com>
2020-08-16 15:23:27 -0300

    (vue-router) navegate route from methods

--- vue-router/src/components/user/Profile.vue
+++ vue-router/src/components/user/Profile.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>User profile</h1>
+  <div class="usuario">
+    <h1>User profile</h1>
+    <hr>
+    <button sucesso @click="goHomepage">Back</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    goHomepage() {
+      this.$router.push('/')
+      // this.$router.push({ path: '/' })
+    }
+  }
 }
 </script>
 
