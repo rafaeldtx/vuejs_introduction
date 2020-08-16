e75fef94b470e41300bf1a5194f477d6095567f7
Rafael Domingues <rafael991_@hotmail.com>
2020-08-16 15:17:46 -0300

    (router-link) using active-class with exact path

--- vue-router/src/components/template/Menu.vue
+++ vue-router/src/components/template/Menu.vue
@@ -1,8 +1,12 @@
 <template>
   <nav class="menu">
     <ul>
-      <router-link to="/" tag="li"><a>Home Page</a></router-link>
-      <router-link to="/user/profile" tag="li"><a>User Profile</a></router-link>
+      <router-link to="/" tag="li" active-class="active" exact>
+        <a>Home Page</a>
+      </router-link>
+      <router-link to="/user/profile" tag="li" active-class="active">
+        <a>User Profile</a>
+      </router-link>
     </ul>
   </nav>
 </template>
