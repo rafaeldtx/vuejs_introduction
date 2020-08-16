096b118c3f6e6c31a57c1d8e9f14e2d09154a48c
Rafael Domingues <rafael991_@hotmail.com>
2020-08-16 15:06:57 -0300

    (vue-router) add router-link menu

---
+++ vue-router/src/components/template/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="menu">
+    <router-link to="/">Home Page</router-link>
+    <router-link to="/user/profile">User Profile</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
