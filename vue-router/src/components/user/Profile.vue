f5e4a915722e9c2c683e3141f505adc1ca05a062
Rafael Domingues <rafael991_@hotmail.com>
2020-08-16 15:36:40 -0300

    (vue-router) add watch for url params

--- vue-router/src/components/user/Profile.vue
+++ vue-router/src/components/user/Profile.vue
@@ -14,6 +14,11 @@ export default {
       id: this.$route.params.id
     }
   },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id
+    }
+  },
   methods: {
     goHomepage() {
       this.$router.push('/')
