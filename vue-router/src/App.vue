c8a6ad5c9619eede2721f57720e0f4e17ce63524
Rafael Domingues <rafael991_@hotmail.com>
2020-08-22 12:06:25 -0300

    (vue-router) using route with transition animation

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -3,7 +3,11 @@
 		<h1>Rotas com VueRouter</h1>
 
     <router-view name="menu" />
-    <router-view />
+    <transition mode="out-in"
+      enter-active-class="animated rubberBand"
+      leave-active-class="animated rollOut">
+      <router-view />
+    </transition>
 	</div>
 </template>
 
