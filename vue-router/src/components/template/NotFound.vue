8db342141d1e40b91f127265a78b53f9a2dd0e7b
Rafael Domingues <rafael991_@hotmail.com>
2020-08-22 11:55:43 -0300

    (vue-router) create NotFounf component

---
+++ vue-router/src/components/template/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="not-found">
+    <h1><strong>404</strong> Page not found</h1>
+    <return-button path="/" />
+  </div>
+</template>
+
+<script>
+import ReturnButton from '@/components/template/ReturnButton'
+export default {
+  components: { ReturnButton }
+}
+</script>
