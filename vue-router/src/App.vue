22134fd64f9a4bfa581fd669fb4b348b121d3ab3
Rafael Domingues <rafael991_@hotmail.com>
2020-09-19 18:26:17 -0300

    (Router/Hash) Add link-to to scroll back on top

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="app">
-		<h1>Rotas com VueRouter</h1>
+		<h1 id="header-title">Rotas com VueRouter</h1>
 
     <router-view name="menu" />
     <transition mode="out-in"
@@ -10,14 +10,28 @@
     </transition>
 
     <div id="footer">
-      <h6>Copyright &copy; Rafael Domingues Teixeira</h6>
+      <div class="message">
+        <p class="description">
+          Parece que não há muito o que fazer por aqui, click no botão abaixo e continue a explorar :D
+        </p>
+        <router-link
+        :to="'#header-title'"
+        :v-on:click="scrollTop"
+        class="scroll-button">Ir para o topo</router-link>
+      </div>
+
+      <h6 class="copyright">Copyright &copy; Rafael Domingues Teixeira</h6>
     </div>
 	</div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    scrollTop() {
+      location.hash = '#header-title';
+    }
+  }
 }
 </script>
 
@@ -30,5 +44,29 @@ export default {
 
   #footer {
     margin-top: 1000px;
+  }
+
+  .message {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .message .description {
+    margin-bottom: 1.5rem;
+    max-width: 500px;
+    text-align: center;
+  }
+
+  .message .scroll-button {
+    background-color: rgb(119, 175, 109);
+    color: rgb(41, 80, 34);
+    border-radius: 10px;
+    padding: 1.5rem;
+    text-align: center;
+  }
+
+  #footer .copyright {
+    text-align: center;
   }
 </style>
